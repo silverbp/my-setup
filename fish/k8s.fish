9c726a27ee96ea463a4e81da8d33174f44273938
Casey Entzi <casey.entzi@unanet.com>
2026-06-15 11:32:45 -0500

    chore(fish): add trailing newline to k8s aliases file

--- fish/k8s.fish
+++ fish/k8s.fish
@@ -2,3 +2,4 @@ alias kube-cleanup-pods 'kubectl get po --all-namespaces --field-selector "statu
 alias kube-diag 'kubectl run tmp-shell-centzi --rm -i --tty --image nicolaka/netshoot -- /bin/bash'
 alias kube-win-cmd 'kubectl run win-tmp-shell --rm -i --tty --image mcr.microsoft.com/windows/servercore:1809 --overrides='"'"'{"apiVersion":"v1","spec":{"imagePullSecrets":[{"name":"docker-cfg"}],"nodeSelector":{"kubernetes.io/arch":"amd64","kubernetes.io/os":"windows"},"tolerations":[{"effect":"NoSchedule","key":"os","operator":"Equal","value":"windows"}]}}'"'"' -- powershell.exe'
 alias kube-psql 'kubectl run tmp-psql-centzi --rm -i --tty --image governmentpaas/psql'
+
